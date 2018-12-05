511af0510f49f6db6a8ff862d6fe71805e583a92
AhmedAmineDAOU <ahmedaminedaou@gmail.com>
2018-12-05 00:58:06 -0500

    threshold segmentation

--- TpIFT6150-5-A.c
+++ TpIFT6150-5-A.c
@@ -1,6 +1,6 @@
 /*------------------------------------------------------*/
 /* Prog    : ProgDemoN.c                                */
-/* Auteur  :                                            */
+/* Auteur  : Ahmed Amine DAOU & Wrushab WARSHE          */
 /* Date    :                                            */
 /* version :                                            */ 
 /* langage : C                                          */
@@ -27,14 +27,15 @@
 /*------------------------------------------------*/
 /* PROGRAMME PRINCIPAL   -------------------------*/                     
 /*------------------------------------------------*/
-main(int argc, char *argv[])
+
+int main(int argc, char *argv[])
  {
   int i,j;
   int seuilMV;
   int length,width;
 
-  float** y;  /* image a segmenter */
-  float** x;  /* champ d'etiquettes a estimer */
+  float** y ;  /* image a segmenter */
+  float** x  ;  /* champ d'etiquettes a estimer */
   float*  VectHist;
 
  
@@ -45,20 +46,32 @@ main(int argc, char *argv[])
  
   printf("Entrez un seuil: ");
   scanf("%d",&seuilMV);
- 
+  /*y= image passee en argument*/
   /* Ouvrir l'image d'entree */
+  y = LoadImagePgm(argv[argc - 1], &length, &width);
+  x = fmatrix_allocate_2d(length, width);
+  /* Seuil */
+  for( i=0 ; i < length ; i++ )
+    for( j=0 ; j < width ; j++ ) {
+      if ( y[i][j] > seuilMV )//if pixel > seuil 
+            x[i][j]=0;// pixel est blanc
+      else  x[i][j]=255;
+           
+      }
 
-	/* Seuil */
-
-  /* Sauvegarde du champ d'etiquettes et de l'histogramme de l'image d'entree */
-
+  float* histo = malloc(sizeof(float) * 255);
+  compute_histo(y, length, width, histo);
+  SaveHistoPgm(NAME_IMG_OUT2, histo);
+    
   /* Liberer la memoire des images */
-
+  free_fmatrix_2d(x);
+  free_fmatrix_2d(y);
   /*Commande systeme: visualisation de Ingout.pgm*/
   system("display A-segmentation.pgm&"); 
   system("display A-histogramme.pgm&");
   
   /*retour sans probleme*/ 
   printf("\n C'est fini ... \n\n\n");
-  return 0; 	 
+  return 0;    
+  	 
 }
